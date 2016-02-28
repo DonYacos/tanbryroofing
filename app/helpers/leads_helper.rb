acffd53c17bd2aa1206d12aa1f2f208047fbc4ec
Don Yacos <donaldyacos@gmail.com>
2016-02-28 17:29:33 -0600

    Add Call and Map buttons to lead email

--- app/helpers/leads_helper.rb
+++ app/helpers/leads_helper.rb
@@ -1,2 +1,8 @@
 module LeadsHelper
+
+	def address_for_google_maps(lead)
+		@lead = lead
+		address = "#{lead.streetnumber} #{lead.street}, #{lead.city}, #{lead.state} #{lead.zip}"
+		address.split.map { |e| "+" + e }.join
+	end
 end
